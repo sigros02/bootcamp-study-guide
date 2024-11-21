8b475c275cbf608db08195d64135518000ef1414
sigros02 <sigros02@gmail.com>
2024-11-20 22:09:31 -0500

    Added script.js file and linked it to index.html

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+const shapes = ["triangle", "square", "pentagon", "circle"];
+
+for (let shapeArrayIndex = 0; shapeArrayIndex < shapes.length; shapeArrayIndex++ ) {
+    console.log(shapes[shapeArrayIndex]);
+}
+
+const topic = "HTML";
+
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
